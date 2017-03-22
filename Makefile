27b7deb6306be4edef2488901e06d56993261d48
Daniel Esteban Maida <daniel.maida@wsu.edu>
2017-03-22 11:09:09 -0700

    Finished working on error checking

--- Makefile
+++ Makefile
@@ -1,13 +1,10 @@
-#OPTIMIZE=-03 -DNDEBUG
-DEBUGGABLE=-g
-CFLAGS = -std=c99 -Wall -pedantic $(DEBUGGABLE) $(OPTIMIZE) -c -g
-philo: random.o philo.o
-	gcc -pthread random.o philo.o -o philo -lm
-philo.o: philo.c random.h
-	gcc $(CFLAGS) philo.c
 
-random.o: random.c random.h
-	gcc $(CFLAGS) random.c
+DEBUGGABLE=-g
+CFLAGS = -std=c99 -Wall -pedantic $(DEBUGGABLE) -c -g
+philo: philo.o
+	gcc -pthread philo.o -o philo -lm
+philo.o: philo.c
+	gcc $(CFLAGS) philo.c
 
 clean:
 	rm -f *.o philo
--- philo.c
+++ philo.c
@@ -17,23 +17,30 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/stat.h>
-
-#include "random.h"
-
+#include <math.h>
 #define NUM_PHILO 5
+
 int chopsticks;
 
+int randomGaussian(int mean, int stddev) {
+    double mu = 0.5 + (double) mean;
+    double sigma = fabs((double) stddev);
+    double f1 = sqrt(-2.0 * log((double) rand() / (double) RAND_MAX));
+    double f2 = 2.0 * 3.14159265359 * (double) rand() / (double) RAND_MAX;
+    if (rand() & (1 << 5)) 
+        return (int) floor(mu + sigma * cos(f2) * f1);
+    else            
+        return (int) floor(mu + sigma * sin(f2) * f1);
+}
 
-void EatOrThink(int i, pid_t pid){
-
-    //printf("Philosopher %i starting (process %d)\n", i, pid);
-
+void EatOrThink(int i){
+    srand(i);
     int time_eating = 0;
     int time_thinking = 0;
 
-    while(time_eating < 25){
+    while(time_eating < 100){
 
-        int time_to_think = randomGaussian(5, 3); // 11 , 7
+        int time_to_think = randomGaussian(11, 7);
         if (time_to_think < 0){
             time_to_think = 0;
         }
@@ -45,8 +52,7 @@ void EatOrThink(int i, pid_t pid){
         int left = i;
         int right = (i + 1) % NUM_PHILO;
 
-        printf("Philosopher %i checking for chopsticks %i and %i\n",
-            i,left,right );
+        printf("Philosopher %i checking for chopsticks %i and %i\n", i,left,right);
 
         struct sembuf take[2];
         take[0].sem_num = left;
@@ -65,43 +71,43 @@ void EatOrThink(int i, pid_t pid){
         drop[1].sem_flg = 0;        
 
         int time_to_eat = 0;
+        int errCheck = 0;
 
-        if (semop(chopsticks, take, 2) == 0) {
-
-        	time_to_eat = randomGaussian(9, 3); // 9 , 3
+        if ((errCheck = semop(chopsticks, take, 2)) == 0) { 
+        	time_to_eat = randomGaussian(9, 3);
             if (time_to_eat < 0){
                 time_to_eat = 0;
             }
-            printf("Philosopher %i eating for %i seconds (total %i)\n", 
-                i, time_to_eat, time_eating);
+            printf("Philosopher %i eating for %i seconds (total %i)\n", i, time_to_eat, time_eating);
             sleep(time_to_eat);
             time_eating += time_to_eat;
             semop(chopsticks, drop, 2);
+        } 
+        if (errCheck == -1) {
+            fprintf(stderr, "%s\n", "Semop returned an error");
         }
     }
     printf("Philosopher %i ate for a total of %i seconds and thought for %i seconds\n", i, time_eating, time_thinking);
-
 }
 
 void philo(){
 
     pid_t pid[NUM_PHILO];
    
-    chopsticks = semget(IPC_PRIVATE, NUM_PHILO, IPC_CREAT | 0666);  //IPC used by process related to a fork, IPC_PRIVATE, IPC_CREAT | 0666 -permissions for server/any rw access
-    for(int i=0; i< NUM_PHILO; i++){
-        semctl(chopsticks, i, SETVAL, 1);  //Go through and set the valud of the chopstick to 1
+    chopsticks = semget(IPC_PRIVATE, NUM_PHILO, IPC_CREAT | 0666);
+    
+    for(int i=0; i< NUM_PHILO; i++){ // set the values of the semaphore
+        semctl(chopsticks, i, SETVAL, 1);  
     }
 
     for (int i = 0; i < NUM_PHILO; ++i){
         pid_t childID = fork();
-        //printf("Forked Philosopher %i (process %d)\n", i, childID);
         pid[i] = childID;
         if (pid[i] < 0){
             printf("Error\n");
             exit(1);
         }else if (pid[i] == 0){
-            //printf("Child (%d): %d\n", i, getpid());
-            EatOrThink(i, childID);
+            EatOrThink(i);
             printf("Philosopher %i is leaving the table\n", i);
             exit(1);
         }        
@@ -109,8 +115,10 @@ void philo(){
     for (int i = 0; i < NUM_PHILO; ++i){
         wait(NULL);
     }
+    if (semctl(chopsticks, NUM_PHILO, IPC_RMID) < 0) {
+        fprintf(stderr, "%s\n", "Error in semctl");
+    }
 }
-
 
 int main(int argc, char* argv[]){
     philo();
